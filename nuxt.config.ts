a80d1c5a424690d970760a46f8d80f3e1aa40c23
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2025-01-07 10:15:30 +0100

    remove sanity from project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,20 +8,8 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@vee-validate/nuxt',
     '@nuxtjs/i18n',
-    '@nuxtjs/sanity',
     "@vueuse/nuxt",
   ],
-  sanity: {
-    projectId: process.env.NUXT_SANITY_PROJECT_ID,
-    dataset: process.env.NUXT_SANITY_DATASET,
-    useCdn: true, // `false` if you want to ensure fresh data
-    apiVersion: process.env.NUXT_SANITY_API_VERSION || '2024-03-15',
-    visualEditing: {
-      studioUrl: process.env.NUXT_SANITY_STUDIO_URL || 'http://localhost:3333',
-      token: process.env.NUXT_SANITY_API_READ_TOKEN,
-      stega: true,
-    },
-  },
   i18n: {
     locales: ['en', 'pl'], // used in URL path prefix
     strategy: 'prefix_except_default',
